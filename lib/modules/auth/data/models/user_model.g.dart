3635be3e432955a0a55b35363596f2f432ccaac1
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2023-07-11 12:42:29 +0200

    ..fix

--- lib/modules/auth/data/models/user_model.g.dart
+++ lib/modules/auth/data/models/user_model.g.dart
@@ -10,13 +10,11 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
       uid: json['uid'] as String,
       email: json['email'] as String,
       isAdmin: json['isAdmin'] as bool? ?? false,
-      errorMessage: json['errorMessage'] as String? ?? '',
     )..name = json['name'] as String?;
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
       'uid': instance.uid,
       'name': instance.name,
       'email': instance.email,
-      'errorMessage': instance.errorMessage,
       'isAdmin': instance.isAdmin,
     };
